1e391f149a0e67220ebe8f1ea2be3c912bbbedc2
kristy121327 <kristy121327@mail.ru>
2025-05-27 16:42:31 +0700

    переместила css в styles

--- frontend/src/pages/BooksPage.js
+++ frontend/src/pages/BooksPage.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
 import { useAuth } from "../AuthContext"; // путь уточни
-import "./BooksPage.css";
+import '../styles/BooksPage.css';
 
 const BooksPage = () => {
   const [books, setBooks] = useState([]);
--- frontend/src/pages/MyBooksPage.js
+++ frontend/src/pages/MyBooksPage.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
-import "./MyBooksPage.css";
+import "../styles/MyBooksPage.css";
 import { Modal, ThemeToggle, ProfileButton } from "../components";
 
 const BooksPage = () => {
